7b4e3347521b872259a374295cf3c702d359e035
Ronja <axoila@gmx.de>
2017-03-22 16:13:46 +0100

    Basic Walking

--- EdgyKidd/Assets/Scripts/Player/PlayerController.cs
+++ EdgyKidd/Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float speed = 10;
 
+    private Rigidbody2D rigid;
+
+    private 
+
+    void Awake () {
+        rigid = GetComponent<Rigidbody2D> ();
+    }
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	// PhysicsTick
+	void FixedUpdate () {
+        Vector2 velocity = rigid.velocity;
+        velocity.x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+        rigid.velocity = velocity;
+
+
 	}
 }
