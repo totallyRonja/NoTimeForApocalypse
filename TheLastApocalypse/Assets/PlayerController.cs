318c7bc858668ae92052d840fceca5d15543e1bc
Ronja <axoila@gmx.de>
2017-03-27 21:51:44 +0200

    start new game

---
+++ TheLastApocalypse/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float walkSpeed = 1;
+
+	Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Awake () {
+		rigid = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement();
+	}
+
+	private void Movement() {
+		Vector2 velocity = new Vector2 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * walkSpeed;
+		rigid.velocity = velocity;
+	}
+}
