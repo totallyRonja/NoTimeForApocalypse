a47a36c3ba063e96a2fca56bcc747ffd64d5fe3c
Ronja <axoila@gmx.de>
2017-03-29 22:20:03 +0200

    implemented super simple fighing system

---
+++ NoTimeForApocalypse/Assets/HitParticle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitParticle : MonoBehaviour {
+
+
+	public float initLifetime = 1;
+	public float speed = 5;
+
+	private float lifetime;
+
+	// Use this for initialization
+	void Start () {
+		lifetime = initLifetime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.up * Time.deltaTime * speed;
+
+		transform.localScale = Vector3.one * (1.5f - 1.0f * (lifetime/initLifetime));
+		lifetime -= Time.deltaTime;
+		if (lifetime <= 0) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		//TODO punch stuff
+	}
+}
--- NoTimeForApocalypse/Assets/PlayerController.cs
+++ NoTimeForApocalypse/Assets/PlayerController.cs
@@ -5,21 +5,48 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float walkSpeed = 1;
+	public GameObject hitPrefab;
+	public GameObject hitOrigin;
 
 	Rigidbody2D rigid;
+	Transform visualization;
+
+	float direction = 0;
+	float hitCooldown = 0;
 
 	// Use this for initialization
 	void Awake () {
 		rigid = GetComponent<Rigidbody2D> ();
+		visualization = transform.FindChild ("Sprite");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Movement();
+		Punch();
 	}
 
 	private void Movement() {
 		Vector2 velocity = new Vector2 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * walkSpeed;
 		rigid.velocity = velocity;
+
+		if (velocity != Vector2.zero) {
+			direction = Mathf.Atan2 (-velocity.x, velocity.y);
+			//Debug.Log (direction);
+
+			if (velocity.x > 0) {
+				visualization.localScale = new Vector3 (1, 1, 1);
+			} else if (velocity.x < 0) {
+				visualization.localScale = new Vector3 (-1, 1, 1);
+			}
+		}
+	}
+
+	private void Punch() {
+		hitCooldown -= Time.deltaTime;
+		if (Input.GetButtonDown ("Fire1") && hitCooldown < 0) {
+			GameObject newHit = Instantiate(hitPrefab, hitOrigin.transform.position, Quaternion.AngleAxis(direction * Mathf.Rad2Deg, Vector3.forward), transform);
+			hitCooldown = 0.4f;
+		}
 	}
 }
