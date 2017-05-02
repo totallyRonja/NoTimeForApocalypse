40e153220ef536812fa3313199607648241a5256
Ronja <axoila@gmx.de>
2017-05-02 11:49:33 +0200

    animated bushes with it's own animator cause fuck unity animation stuff

--- NoTimeForApocalypse/Assets/MainIsland/Foilage/GenerateFoliage.cs
+++ NoTimeForApocalypse/Assets/MainIsland/Foilage/GenerateFoliage.cs
@@ -9,6 +9,8 @@ public class GenerateFoliage : MonoBehaviour {
     public Vector2 amount = Vector2.one * 2;
     public Vector2 random;
     public Sprite[] sprites;
+    public Texture2D[] spritesheets;
+    Sprite[][] sheetframes;
 
     private Collider2D coll;
 
@@ -20,20 +22,33 @@ public class GenerateFoliage : MonoBehaviour {
     void Start() {
         Stopwatch sw = Stopwatch.StartNew();
         Generate();
-        print("generated scrubs in" + sw.ElapsedMilliseconds);
+        print(this + " generated scrubs in " + sw.ElapsedMilliseconds);
     }
 
     void Generate() {
         //delete existing children
         //for (int i = 0; i < transform.childCount; i++)
         //    Destroy(transform.GetChild(i).gameObject);
-
+        if(spritesheets != null && spritesheets.Length > 0 && (sheetframes == null || sheetframes.Length == 0)) {
+            sheetframes = new Sprite[spritesheets.Length][];
+            for(int i = 0; i < spritesheets.Length; i++) {
+                sheetframes[i] = new Sprite[4];
+                for (int j=0;j<4;j++) {
+                    sheetframes[i][j] = Sprite.Create(spritesheets[i], new Rect(0, j * spritesheets[i].height / 4, spritesheets[i].width, spritesheets[i].height / 4), new Vector2(0.5f, 0f));
+                }
+            }
+        }
+        
         for (float x = coll.bounds.center.x - coll.bounds.extents.x; x <= coll.bounds.center.x + coll.bounds.extents.x; x += coll.bounds.extents.x * 2 / (amount.x - 1)) {
-            for (float y = coll.bounds.center.y - coll.bounds.extents.y; y <= coll.bounds.center.y + coll.bounds.extents.y; y += coll.bounds.extents.y * 2 / (amount.y - 1)) {
+            for (float y = coll.bounds.center.y + coll.bounds.extents.y; y >= coll.bounds.center.y - coll.bounds.extents.y; y -= coll.bounds.extents.y * 2 / (amount.y - 1)) {
                 Vector2 newPos = new Vector2(x+Random.Range(-random.x, random.x), y + Random.Range(-random.y, random.y));
                 if (coll.OverlapPoint(newPos)) {
                     GameObject newScrub = Instantiate(protoScrub, newPos, transform.rotation, transform);
-                    newScrub.GetComponent<SpriteRenderer>().sprite = sprites[(int)(sprites.Length*Random.value)];
+                    if (spritesheets.Length == 0) {
+                        newScrub.GetComponent<SpriteRenderer>().sprite = sprites[(int)(sprites.Length * Random.value)];
+                    } else {
+                        newScrub.GetComponent<SpriteAnimator>().setSprites(sheetframes[(int)(spritesheets.Length * Random.value)]);
+                    }
                 }
             }
         }
