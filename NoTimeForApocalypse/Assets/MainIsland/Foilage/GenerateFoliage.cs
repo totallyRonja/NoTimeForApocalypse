b33e6fca118ac26e64512f45900229d233ce391d
Ronja <axoila@gmx.de>
2017-05-09 01:32:29 +0200

    try to rewrite grass generation

--- NoTimeForApocalypse/Assets/MainIsland/Foilage/GenerateFoliage.cs
+++ NoTimeForApocalypse/Assets/MainIsland/Foilage/GenerateFoliage.cs
@@ -10,7 +10,18 @@ public class GenerateFoliage : MonoBehaviour {
     public Vector2 random;
     public Sprite[] sprites;
 
+    public Rect offset_size; //offset AND size
+
     private Collider2D coll;
+
+    new Transform camera;
+    Vector2 cameraHalfExtents;
+
+    Rect indexBounds;
+
+    int tickCounter = 0;
+
+    Dictionary<Vector2, GameObject> existingObjects = new Dictionary<Vector2, GameObject>();
 
     private void Awake() {
         coll = GetComponent<Collider2D>();
@@ -18,13 +29,48 @@ public class GenerateFoliage : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-        Stopwatch sw = Stopwatch.StartNew();
+        camera = Camera.main.transform;
+        cameraHalfExtents = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+
+        
+
+        //Stopwatch sw = Stopwatch.StartNew();
         Generate();
-        print(this + " generated scrubs in " + sw.ElapsedMilliseconds);
+        //print(this + " generated scrubs in " + sw.ElapsedMilliseconds);
+    }
+
+    private void Update() {
+        if (tickCounter % 6 == 0) {
+            Generate();
+        }
+        tickCounter++;
     }
 
     void Generate() {
-        //delete existing children
+        existingObjects.Clear();
+        int i = 0;
+
+        Vector2 indexOriginPos = new Vector2((camera.position.x - cameraHalfExtents.x - offset_size.x - coll.bounds.min.x) * amount.x / coll.bounds.size.x,
+                (camera.position.y - cameraHalfExtents.y - offset_size.y - coll.bounds.min.y) * amount.y / coll.bounds.size.y);
+        indexBounds = new Rect(Mathf.Floor(indexOriginPos.x), Mathf.Floor(indexOriginPos.y),
+                Mathf.Floor((cameraHalfExtents.x * offset_size.width) * amount.x / coll.bounds.size.x), 
+                Mathf.Floor((cameraHalfExtents.y * offset_size.height) * amount.y / coll.bounds.size.x));
+
+
+        for(int x = 0; x < indexBounds.width; x++) {
+            for (int y = 0; y < indexBounds.height; y++) {
+                Vector2 indexBasePos = new Vector2(indexBounds.x + x, indexBounds.y + y);
+                Vector2 basePos = new Vector2(indexBasePos.x * coll.bounds.size.x / amount.x + coll.bounds.min.x,
+                        indexBasePos.y * coll.bounds.size.y / amount.y + coll.bounds.min.x);
+
+                existingObjects[basePos] = null;
+
+                i++;
+            }
+        }
+        //print(i);
+
+        /*//delete existing children
         //for (int i = 0; i < transform.childCount; i++)
         //    Destroy(transform.GetChild(i).gameObject);
         
@@ -37,6 +83,14 @@ public class GenerateFoliage : MonoBehaviour {
                     newScrub.GetComponent<SpriteRenderer>().sprite = sprites[(int)(sprites.Length * Random.value)];
                 }
             }
+        }*/
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.blue;
+        foreach (Vector2 pos in existingObjects.Keys) {
+            Gizmos.DrawSphere(pos, 0.5f);
+            //print(pos);
         }
     }
 }
