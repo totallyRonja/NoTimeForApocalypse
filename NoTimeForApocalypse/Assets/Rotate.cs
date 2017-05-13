fa27272b68f6b71ec1958cd41792ff2878c68e31
Ronja <axoila@gmx.de>
2017-05-13 17:17:41 +0200

    improved main menu, added beta 0.1 versio

---
+++ NoTimeForApocalypse/Assets/Rotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class Rotate : MonoBehaviour {
+
+	public float rps;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, 0, Time.deltaTime * rps * 360);
+	}
+
+	void OnEnable(){
+		EditorApplication.update += Update;
+	}
+	void OnDisable(){
+		EditorApplication.update -= Update;
+	}
+}
