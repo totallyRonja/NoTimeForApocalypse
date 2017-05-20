5f2dd253898a49771372172ed5723103c55fab54
Ronja <axoila@gmx.de>
2017-05-20 11:19:36 +0200

    loading screen

--- NoTimeForApocalypse/Assets/UI/ReloadButton.cs
+++ NoTimeForApocalypse/Assets/UI/ReloadButton.cs
@@ -8,16 +8,18 @@ using UnityEngine.EventSystems;
 public class ReloadButton : MonoBehaviour {
 
     public string scene;
+    public GameObject loadingScreen;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         GetComponent<Button>().onClick.AddListener(OnClick);
 	}
 
     void OnClick() {
         GameObject menu = GameObject.FindWithTag("PauseMenu");
         if (menu) menu.GetComponent<PauseMenu>().Unpause();
-        
+        if (loadingScreen) loadingScreen.SetActive(true);
+
         SceneManager.LoadScene(scene==""?SceneManager.GetActiveScene().name:scene);
     }
 }
